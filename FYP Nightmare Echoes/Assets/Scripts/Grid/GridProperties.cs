37e62eda0deb8e980023f1e8b7bb45ad1706299f
Alex Mah Jia Yong <ihavenofriend1234@gmail.com>
2023-06-01 01:52:51 +0800

    added assembly definitions test

--- FYP Nightmare Echoes/Assets/Scripts/Grid/GridProperties.cs
+++ FYP Nightmare Echoes/Assets/Scripts/Grid/GridProperties.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace NightmareEchoes
+namespace NightmareEchoes.Grid
 {
-    public class MouseCursor : MonoBehaviour
+    public class GridProperties : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
