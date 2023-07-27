2ec6c54d8ac9b61a474df9865323d881a7630c31
Alex Mah Jia Yong <ihavenofriend1234@gmail.com>
2023-07-27 21:34:48 +0800

    Updated sorting layer
    - Units use unit layer
    - tiles related use tile layer

--- FYP Nightmare Echoes/Assets/Scripts/Grid/VinnaeyshGridStuff/OverlayTile.cs
+++ FYP Nightmare Echoes/Assets/Scripts/Grid/VinnaeyshGridStuff/OverlayTile.cs
@@ -26,12 +26,12 @@ namespace NightmareEchoes.Grid
 
         public void ShowTile()
         {
-            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1.5f, 1);
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1.5f, 0.5f);
         }
 
         public void HideTile()
         {
-            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1.5f, 0);
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1.5f, 0f);
 
         }
     }
