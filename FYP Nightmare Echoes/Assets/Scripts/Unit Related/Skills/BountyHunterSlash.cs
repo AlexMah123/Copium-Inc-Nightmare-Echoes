63819180be014ac205def094eea3f92d45c43b87
Vinnaeysh1507 <75375348+Vinnaeysh@users.noreply.github.com>
2023-09-18 16:00:07 +0800

    Bounty Hunter WIP

---
+++ FYP Nightmare Echoes/Assets/Scripts/Unit Related/Skills/BountyHunterSlash.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using NightmareEchoes.Grid;
+using NightmareEchoes.Unit.Combat;
+using UnityEngine;
+
+//Created by Vinn
+namespace NightmareEchoes.Unit
+{
+    public class BountyHunterSlash : Skill
+    {
+        public override bool Cast(Units target)
+        {
+            target.TakeDamage(damage);
+            return true;
+        }
+    }
+}
