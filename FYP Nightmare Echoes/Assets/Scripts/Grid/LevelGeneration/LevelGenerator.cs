12a26ebefad284c09a37672dda2daeb36a6cc29d
Meh1928 <thejedi64@gmail.com>
2023-09-23 19:56:34 +0800

    WIP - not working atm just saving progress

--- FYP Nightmare Echoes/Assets/Scripts/Grid/LevelGeneration/LevelGenerator.cs
+++ FYP Nightmare Echoes/Assets/Scripts/Grid/LevelGeneration/LevelGenerator.cs
@@ -14,12 +14,13 @@ namespace NightmareEchoes.Grid
     {
         private string path = "Assets\\Scripts\\Grid\\LevelGeneration\\RoomPresets.txt";
 
-        public List<int[,]> rooms = new();
-        
+        //public List<int[,]> rooms = new();
+        public Dictionary<string, List<int[,]>> rooms = new();
+
         public TileBase testTile;
         public Tilemap tilemap;
 
-        public void UpdateRooms()
+        public void ReadRoomsFromFile()
         {
             rooms.Clear();
             
@@ -29,6 +30,8 @@ namespace NightmareEchoes.Grid
             var roomX = 0;
             var roomZ = 0;
             string room = null;
+            string roomType = null;
+            List<int[,]> roomList = new();
 
             var debugTermintate = false;
             
@@ -39,7 +42,19 @@ namespace NightmareEchoes.Grid
                 if (debugTermintate) break;
 
                 if (output.Length <= 0) continue;
-                if (output[0] == '[') continue;
+                if (output[0] == '[')
+                {
+                    if (roomType != null)
+                    {
+                        var cloneList = new List<int[,]>();
+                        cloneList = roomList;
+                        rooms.Add(roomType, cloneList);
+                        roomList.Clear();
+                    }
+
+                    roomType = output.Trim('[',']');
+                    continue;
+                }
                 
                 switch (output)
                 {
@@ -48,7 +63,8 @@ namespace NightmareEchoes.Grid
                         continue;
                     case "#!":
                         parseRoom = false;
-                        SaveRoom(room, roomX, roomZ);
+                        var roomMatrix = ParseRoom(room, roomX, roomZ);
+                        roomList.Add(roomMatrix);
                         roomX = 0;
                         roomZ = 0;
                         room = null;
@@ -65,10 +81,12 @@ namespace NightmareEchoes.Grid
                 roomZ++;
                 room += $"{output}\n";
             }
+            
+            rooms.Add(roomType, roomList);
             streamReader.Close();  
         }
 
-        private void SaveRoom(string room, int x, int z)
+        private int[,] ParseRoom(string room, int x, int z)
         {
             var roomMatrix = new int[x, z];
             room = room.Replace("\n", "");
@@ -78,7 +96,6 @@ namespace NightmareEchoes.Grid
             {
                 for (var j = 0; j < z; j++)
                 {
-                    //Debug.Log($"{i},{j},{room[count]}");
                     roomMatrix[i, j] = room[count] switch
                     {
                         '.' => 1,
@@ -88,21 +105,21 @@ namespace NightmareEchoes.Grid
                     count++;
                 }
             }
-            
-            rooms.Add(roomMatrix);
+
+            return roomMatrix;
         }
 
-        public void PickRandomRoom()
+        public void PickRandomRoom(string category)
         {
-            var rand = Random.Range(0, rooms.Count);
+            var rand = Random.Range(0, rooms[category].Count);
 
-            var selectedRoom = rooms[rand];
+            var selectedRoom = rooms[category][rand];
 
             tilemap.ClearAllTiles();
             
-            for (int x = 0; x <= selectedRoom.GetUpperBound(0) ; x++)
+            for (var x = 0; x <= selectedRoom.GetUpperBound(0) ; x++)
             {
-                for (int z = 0; z <= selectedRoom.GetUpperBound(1); z++)
+                for (var z = 0; z <= selectedRoom.GetUpperBound(1); z++)
                 {
                     var tilelocation = new Vector3Int(x, z, 0);
                     if (selectedRoom[x, z] == 1)
@@ -110,6 +127,14 @@ namespace NightmareEchoes.Grid
                         tilemap.SetTile(tilelocation, testTile);
                     }
                 }
+            }
+        }
+
+        public void DebugRooms(string category)
+        {
+            foreach (var kvp in rooms)
+            {
+                Debug.Log(rooms[category].Capacity);
             }
         }
         
@@ -124,15 +149,28 @@ namespace NightmareEchoes.Grid
 
             var generator = (LevelGenerator)target;
 
-            if (GUILayout.Button("Test"))
+            if (GUILayout.Button("Update Rooms From File"))
             {
-                generator.UpdateRooms();
-            }
-            if (GUILayout.Button("PickRandonRoom"))
-            {
-                generator.PickRandomRoom();
+                generator.ReadRoomsFromFile();
             }
             
+            GUILayout.Label("PickRandomRoom");
+            if (GUILayout.Button("Large"))
+            {
+                generator.PickRandomRoom("LARGE");
+            }
+            if (GUILayout.Button("Medium"))
+            {
+                generator.PickRandomRoom("MEDIUM");
+            }
+            if (GUILayout.Button("Small"))
+            {
+                generator.PickRandomRoom("SMALL");
+            }
+            
+            GUILayout.Label("Debug");
+            if (GUILayout.Button("Debug"))
+                generator.DebugRooms("LARGE");
         }
     }
     
