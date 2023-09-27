c7e5e39d51364d5c63ecd50bd50695ccfee4cc30
Vinnaeysh1507 <75375348+Vinnaeysh@users.noreply.github.com>
2023-09-27 22:50:18 +0800

    Defender works just offset Knockback stuff

--- FYP Nightmare Echoes/Assets/Scripts/Unit Related/Skills/Hero/Defender/Charge.cs
+++ FYP Nightmare Echoes/Assets/Scripts/Unit Related/Skills/Hero/Defender/Charge.cs
@@ -1,3 +1,4 @@
+using NightmareEchoes.Grid;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,31 @@ namespace NightmareEchoes.Unit
 {
     public class Charge : Skill
     {
-        
+        public override bool Cast(Units unit)
+        {
+            base.Cast(unit);
+            DealDamage(unit);
+
+            var direction = unit.transform.position - transform.position;
+            var destination = unit.transform.position + direction ;
+
+            var tileOccupied = false;
+            var hit = Physics2D.Raycast(destination, Vector2.zero, Mathf.Infinity, LayerMask.GetMask("Overlay Tile"));
+            if (hit)
+            {
+                var tileDestination = hit.collider.gameObject.GetComponent<OverlayTile>();
+                if (tileDestination)
+                {
+                    if (tileDestination.CheckUnitOnTile())
+                        tileOccupied = true;
+                }
+            }
+
+            if (tileOccupied) return true;
+            unit.transform.position += direction;
+            unit.UpdateLocation();
+
+            return true;
+        }
     }
 }
--- FYP Nightmare Echoes/Assets/Scripts/Unit Related/Unit/Hero/DefenderHero.cs
+++ FYP Nightmare Echoes/Assets/Scripts/Unit Related/Unit/Hero/DefenderHero.cs
@@ -43,7 +43,7 @@ namespace NightmareEchoes.Unit
 
         public override void Skill1()
         {
-          
+            CombatManager.Instance.SelectSkill(this, skill1);
         }
 
         public override void Skill2()
