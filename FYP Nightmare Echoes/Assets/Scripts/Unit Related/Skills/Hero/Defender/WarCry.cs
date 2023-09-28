90847acd0b1898dfe28772a9d1bc707103f2c0e4
Vinnaeysh1507 <75375348+Vinnaeysh@users.noreply.github.com>
2023-09-29 00:06:43 +0800

    WAR CRY DONE

--- FYP Nightmare Echoes/Assets/Scripts/Unit Related/Skills/Hero/Defender/WarCry.cs
+++ FYP Nightmare Echoes/Assets/Scripts/Unit Related/Skills/Hero/Defender/WarCry.cs
@@ -1,3 +1,4 @@
+using NightmareEchoes.Grid;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,26 @@ namespace NightmareEchoes.Unit
 {
     public class WarCry : Skill
     {
+        public override bool Cast(OverlayTile target, List<OverlayTile> aoeTiles)
+        {
+            base.Cast(target, aoeTiles);
 
+            var targetType = target.CheckUnitOnTile().GetComponent<Units>();
+
+            if (targetType.IsHostile == false)
+            {
+                targetType.AddBuff(GetStatusEffect.Instance.CreateModifier(STATUS_EFFECT.STRENGTH_TOKEN, 1, 1));
+                Debug.Log("Applying Strength");
+            }
+            if (targetType.IsHostile == true)
+            {
+                targetType.AddBuff(GetStatusEffect.Instance.CreateModifier(STATUS_EFFECT.WEAKEN_TOKEN, 1, 1));
+                Debug.Log("Applying Weaken");
+            }
+
+            return true;
+        }
+
+        
     }
 }
