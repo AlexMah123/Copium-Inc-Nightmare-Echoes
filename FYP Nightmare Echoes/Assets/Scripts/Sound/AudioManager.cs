763e52fe2fae2e3ee0e93993a0b92d54a3654fc6
Vinnaeysh1507 <75375348+Vinnaeysh@users.noreply.github.com>
2023-10-11 17:56:59 +0800

    WIP Audio Stuff

---
+++ FYP Nightmare Echoes/Assets/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NightmareEchoes.Sound
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public Sound[] musicSounds, sfxSounds;
+        public AudioSource musicSources, sfxSource;
+    }
+}
---
+++ FYP Nightmare Echoes/Assets/Scripts/Sound/Sound.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NightmareEchoes.Sound
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string Name;
+        public AudioClip clip;
+    }
+}
